87dc3c06256c6ff5fa323e92e4b21d2b3c1bafba
Manasvi D <manasvidalvi171@gmail.com>
2026-08-05 18:45:32 +0530

    Fix API endpoint URL path

--- backend/backend.js
+++ backend/backend.js
@@ -3,16 +3,12 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import pollRoutes from "./routes/pollRoutes.js";
-
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 // Routes
 app.use("/api/polls", pollRoutes);
 
--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://quickpoll-app-0iep.onrender.com";// Change to your Render URL after backend deploy
+const API_URL = "https://quickpoll-app-0iep.onrender.com/api/polls";
 const activeCharts = {}; 
 const openChartPollIds = new Set();
 
